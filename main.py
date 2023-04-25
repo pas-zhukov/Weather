790fef79ee7c0285ba09428b352e628df86eed4f
Pavel <117192371+pas-zhukov@users.noreply.github.com>
2023-04-25 21:36:06 +0300

    changing variable names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 url_template = 'https://wttr.in/{}'
 
-places = ['Лондон', 'Аэропорт Шереметьево', 'Череповец']
+locations = ['Лондон', 'Аэропорт Шереметьево', 'Череповец']
 
 params = {
     'n': '',
@@ -13,7 +13,7 @@ params = {
     'lang': 'ru'
 }
 
-for place in places:
-    response = requests.get(url_template.format(place), params=params)
+for location in locations:
+    response = requests.get(url_template.format(location), params=params)
     response.raise_for_status()  # Поднимет исключение в случае ответа с кодом типа 4XX или 5ХХ
     print(response.text)
