1d67ca700c1b04fe377f31bdda263a86968e92b6
Pavel <pas-zhukov@yandex.ru>
2023-04-27 00:06:13 +0300

    Updating readme. requirements.txt added.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 url_template = 'https://wttr.in/{}'
 
@@ -17,3 +18,5 @@ for location in locations:
     response = requests.get(url_template.format(location), params=params)
     response.raise_for_status()  # Поднимет исключение в случае ответа с кодом типа 4XX или 5ХХ
     print(response.text)
+
+os.system('pause')  # чтобы консоль не закрывалась
